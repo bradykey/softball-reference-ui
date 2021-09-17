a0dce36488f00f44c897d020f7ca633b87649af1
Brady Key <bradykey@gmail.com>
2021-09-17 16:46:37 -0600

    Added a prettierrc.js file

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'none',
+  singleQuote: true,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  semi: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  endOfLine: 'auto'
+}
