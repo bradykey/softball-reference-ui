c5f01d19599933a103d5b0133522e0f20fdfe4d5
Brady Key <bradykey@gmail.com>
2021-09-25 09:16:13 -0600

    Added helpful isObjectUndefinedEmptyOrNull util method

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,17 @@
+/**
+ * A nice little helper method that checks to see if an object is undefined, empty, or null.
+ * @param {Object} someObject
+ * @returns true if the object is undefined, empty, or null; galse otherwise
+ */
+export function isObjectUndefinedEmptyOrNull(someObject) {
+  // The "truthy" value of null and undefined is FALSE
+  if (someObject) return !Object.keys(someObject).length;
+  else return true;
+}
+
+/**
+ * Constant value that defines the colors used in the theme.
+ */
 export const THEME_COLORS = {
   primary: ' '
 };
