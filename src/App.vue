ba932b7af172d7b79923bd36176144d45ddd189e
Brady Key <bradykey@gmail.com>
2021-09-26 09:42:56 -0600

    Updated logo

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
             alt="Vuetify Logo"
             class="shrink mr-2"
             contain
-            src="@/assets/sr-logo.png"
+            src="@/assets/sr-logo-com.png"
             transition="scale-transition"
             :width="$vuetify.breakpoint.smAndUp ? 250 : 150"
           />
