96f66076a4bd66656ae5d2dfc72011c85432b7ca
Brady Key <bradykey@gmail.com>
2021-10-01 14:46:30 -0600

    Added comments to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    /* Flag to let all components know if a loading bar shoudl be running. */
     isLoading: false
   },
   mutations: {
@@ -13,6 +14,18 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    /**
+     * Commits the SET_IS_LOADING mutation with the passed in Boolean as the
+     * payload.
+     *
+     * @param {Context} context the automatically passed in context object that
+     * contains: state (same as store.state, or local state if in modules),
+     * rootState (same as store.state, only in modules), commite (same as
+     * store.commit), dispatch (same as store.dispatch), getters (same as
+     * store.getters, or local getters if in modules), rootGetters (same as
+     * store.getters, only in modules)
+     * @param {Boolean} isLoading
+     */
     setIsLoading(context, isLoading) {
       context.commit('SET_IS_LOADING', isLoading);
     }
