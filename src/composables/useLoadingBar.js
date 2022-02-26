0722a48e43193eef36d09f4289b476c5473ba865
Brady Key <bradykey@gmail.com>
2022-02-26 01:21:20 -0700

    Added methods.

--- src/composables/useLoadingBar.js
+++ src/composables/useLoadingBar.js
@@ -1,9 +1,17 @@
 import store from '@/store/store';
 
+/**
+ * Composable function that dispatches the VueX action that sets the boolean
+ * value watched by the ProgressBar component to true.
+ */
 export function turnOnLoadingBar() {
   store.dispatch('setIsLoading', true).catch(error => console.log(error));
 }
 
+/**
+ * Composable function that dispatches the VueX action that sets the boolean
+ * value watched by the ProgressBar component to true.
+ */
 export function turnOffLoadingBar() {
   store.dispatch('setIsLoading', false).catch(error => console.log(error));
 }
