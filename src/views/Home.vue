dc33ee6637d96de917f1893daefe60f1a67f020b
Brady Key <bradykey@gmail.com>
2022-10-08 16:50:36 -0600

    Defaulted to Softball-Reference.com team instead of Garden Hosers.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@ export default {
     // immediately re-direct... FOR NOW
     router.push({
       name: 'TeamLeagueSummary',
-      params: { teamName: 'Garden Hosers' }
+      params: { teamName: 'Softball-Reference.com' }
     });
   }
 };
